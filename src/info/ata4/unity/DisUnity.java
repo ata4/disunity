9ffe21d2e8ed53ff47d657d3b6997620e130a0c6
ata4 <barracuda415@yahoo.de>
2013-09-12 01:11:45 +0200

    Updated version

--- src/info/ata4/unity/DisUnity.java
+++ src/info/ata4/unity/DisUnity.java
@@ -34,7 +34,7 @@ public class DisUnity implements Runnable {
 
     private static final Logger L = Logger.getLogger(DisUnity.class.getName());
     
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
     
     private final AssetFileFilter assetFilter = new AssetFileFilter();
     private final DisUnitySettings settings = new DisUnitySettings();
