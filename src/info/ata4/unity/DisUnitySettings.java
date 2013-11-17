177695392e287c478b72cda1c25107db9b7d06cc
ata4 <barracuda415@yahoo.de>
2013-11-17 15:13:16 +0100

    Removed CLI code, will be moved to CLI branch later

--- src/info/ata4/unity/DisUnitySettings.java
+++ src/info/ata4/unity/DisUnitySettings.java
@@ -11,6 +11,7 @@ package info.ata4.unity;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -21,31 +22,14 @@ import java.util.Set;
 public class DisUnitySettings {
     
     private List<File> files = new ArrayList<>();
-    private DisUnityCommand command = DisUnityCommand.EXTRACT;
-    private Set<Integer> classFilter;
+    private Set<Integer> classFilter = new HashSet<>();
     
     public List<File> getFiles() {
         return files;
     }
-
-    public void setFiles(List<File> files) {
-        this.files = files;
-    }
-
-    public DisUnityCommand getCommand() {
-        return command;
-    }
-
-    public void setCommand(DisUnityCommand cmd) {
-        this.command = cmd;
-    }
     
     public Set<Integer> getClassFilter() {
         return classFilter;
-    }
-    
-    public void setClassFilter(Set<Integer> classFilter) {
-        this.classFilter = classFilter;
     }
     
     public boolean isClassFiltered(Integer classID) {
