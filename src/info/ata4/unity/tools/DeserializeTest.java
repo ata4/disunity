7cc4f4bda3152016e9d6bb9bebebb05989d1e87c
ata4 <barracuda415@yahoo.de>
2013-12-06 12:24:35 +0100

    Moved some testing code to new package

---
+++ src/info/ata4/unity/tools/DeserializeTest.java
@@ -0,0 +1,110 @@
+/*
+ ** 2013 August 11
+ **
+ ** The author disclaims copyright to this source code.  In place of
+ ** a legal notice, here is a blessing:
+ **    May you do good and not evil.
+ **    May you find forgiveness for yourself and forgive others.
+ **    May you share freely, never taking more than you give.
+ */
+package info.ata4.unity.tools;
+
+import info.ata4.unity.asset.AssetFile;
+import info.ata4.unity.asset.struct.AssetObjectPath;
+import info.ata4.unity.serdes.Deserializer;
+import info.ata4.unity.serdes.UnityField;
+import info.ata4.unity.serdes.UnityObject;
+import info.ata4.unity.util.ClassID;
+import info.ata4.util.log.LogUtils;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nico Bergemann <barracuda415 at yahoo.de>
+ */
+public class DeserializeTest {
+    
+    private static final Logger L = Logger.getLogger(DeserializeTest.class.getName());
+    
+    public static void main(String[] args) {
+        LogUtils.configure();
+        
+        for (String arg : args) {
+            try {
+                File file = new File(arg);
+
+                AssetFile asset = new AssetFile();
+                asset.load(file);
+
+                Deserializer deser = new Deserializer(asset);
+
+                boolean all = true;
+
+                if (all) {
+                    for (AssetObjectPath path : asset.getObjectPaths()) {
+                        try {
+                            if (path.classID1 < 0) {
+                                continue;
+                            }
+
+                            deser.deserialize(path);
+                        } catch (Exception ex) {
+                            L.log(Level.SEVERE, "Deserialization failed for " + path.pathID + " (" + ClassID.getInstance().getNameForID(path.classID2) + ")", ex);
+                            break;
+                        }
+                    }
+                } else {
+                    try {
+                        AssetObjectPath path = asset.getPathsByID(129).get(0);
+                        UnityObject obj = deser.deserialize(path);
+                        dump(System.out, obj, 0);
+                    } catch (Exception ex) {
+                        L.log(Level.SEVERE, "Deserialization failed", ex);
+                    }
+
+                    try {
+                        AssetObjectPath path = asset.getPathsByID(141).get(0);
+                        UnityObject obj = deser.deserialize(path);
+                        dump(System.out, obj, 0);
+                    } catch (Exception ex) {
+                        L.log(Level.SEVERE, "Deserialization failed", ex);
+                    }
+                }
+            } catch (IOException ex) {
+                L.log(Level.SEVERE, "Can't read asset file", ex);
+            }
+        }
+    }
+    
+    public static void dump(PrintStream ps, UnityObject obj, int level) {
+        if (obj.getName().equals("Base")) {
+            ps.println(obj.getType());
+        } else {
+            ps.println(obj.getType() + " " + obj.getName());
+        }
+        
+        level++;
+
+        for (UnityField field : obj.getFields()) {
+            for (int i = 0; i < level; i++) {
+                ps.print("  ");
+            }
+            
+            String name = field.getName();
+            String type = field.getType();
+            Object value = field.getValue();
+            
+            if (field.getValue() instanceof UnityObject) {
+                dump(ps, (UnityObject) field.getValue(), level);
+            } else if (field.getValue() instanceof String) {
+                ps.printf("%s %s = \"%s\"\n", type, name, value);
+            } else {
+                ps.printf("%s %s = %s\n", type, name, value);
+            }
+        }
+    }
+}
---
+++ src/info/ata4/unity/tools/StructDBStats.java
@@ -0,0 +1,61 @@
+/*
+ ** 2013 Dezember 06
+ **
+ ** The author disclaims copyright to this source code.  In place of
+ ** a legal notice, here is a blessing:
+ **    May you do good and not evil.
+ **    May you find forgiveness for yourself and forgive others.
+ **    May you share freely, never taking more than you give.
+ */
+package info.ata4.unity.tools;
+
+import info.ata4.unity.asset.struct.AssetFieldType;
+import info.ata4.unity.serdes.db.FieldTypeMap;
+import info.ata4.unity.serdes.db.StructDatabase;
+import info.ata4.util.collection.Pair;
+import info.ata4.util.log.LogUtils;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nico Bergemann <barracuda415 at yahoo.de>
+ */
+public class StructDBStats {
+    
+    private static final Logger L = Logger.getLogger(StructDBStats.class.getName());
+    
+    public static void main(String[] args) {
+        LogUtils.configure();
+        
+        FieldTypeMap ftm = StructDatabase.getInstance().getFieldTypeMap();
+        Map<String, AtomicInteger> classCounts = new TreeMap<>();
+        Set<AssetFieldType> fieldNodes = new HashSet<>();
+
+        int classCountTotal = 0;
+        for (Map.Entry<Pair<Integer, String>, AssetFieldType> entry : ftm.entrySet()) {
+            String revision = entry.getKey().getRight();
+            fieldNodes.add(entry.getValue());
+            if (!classCounts.containsKey(revision)) {
+                classCounts.put(revision, new AtomicInteger(1));
+            } else {
+                AtomicInteger value = classCounts.get(revision);
+                value.addAndGet(1);
+                classCountTotal++;
+            }
+        }
+
+        for (Map.Entry<String, AtomicInteger> entry : classCounts.entrySet()) {
+            int classCount = entry.getValue().get();
+            L.log(Level.INFO, "{0}: {1}", new Object[]{entry.getKey(), classCount});
+        }
+
+        L.log(Level.INFO, "Total structs:  {0}", classCountTotal);
+        L.log(Level.INFO, "Unique structs: {0}", fieldNodes.size());
+    }
+}
