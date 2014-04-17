5cdfc989947d12d2575fa4588821149e7586549c
ata4 <barracuda415@yahoo.de>
2014-04-17 03:51:38 +0200

    Updated version

--- src/info/ata4/unity/DisUnity.java
+++ src/info/ata4/unity/DisUnity.java
@@ -16,7 +16,7 @@ package info.ata4.unity;
  */
 public class DisUnity {
 
-    private static final String VERSION = "0.2.2";
+    private static final String VERSION = "0.3.0";
     
     public static String getVersion() {
         return VERSION;
