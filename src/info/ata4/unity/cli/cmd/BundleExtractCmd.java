a67f1fffca9613ad4db0891db3a1e561193d7fb5
ata4 <barracuda415@yahoo.de>
2014-07-02 15:16:08 +0200

    Added --editor parameter

--- src/info/ata4/unity/cli/cmd/BundleExtractCmd.java
+++ src/info/ata4/unity/cli/cmd/BundleExtractCmd.java
@@ -9,6 +9,7 @@
  */
 package info.ata4.unity.cli.cmd;
 
+import com.beust.jcommander.Parameter;
 import info.ata4.io.buffer.ByteBufferUtils;
 import info.ata4.log.LogUtils;
 import info.ata4.unity.asset.bundle.AssetBundle;
@@ -19,12 +20,19 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.FilenameUtils;
 
 /**
  *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
 public class BundleExtractCmd extends AssetCommand {
+    
+    @Parameter(
+        names = "--editor",
+        description = "Prepare extracted files for the Unity editor"
+    )
+    private boolean editor;
     
     private static final Logger L = LogUtils.getLogger();
     
@@ -38,6 +46,11 @@ public class BundleExtractCmd extends AssetCommand {
         for (Map.Entry<String, ByteBuffer> entry : bundle.getEntries().entrySet()) {
             String entryName = entry.getKey();
             ByteBuffer entryBuffer = entry.getValue();
+            
+            // add .unity extension to scene files
+            if (editor && FilenameUtils.getExtension(entryName).isEmpty()) {
+                entryName += ".unity";
+            }
             
             L.log(Level.INFO, "Extracting {0}", entryName);
             
