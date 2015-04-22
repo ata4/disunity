37dc8ead5b5ab2327c55af59cd0419c0585fca95
ata4 <barracuda415@yahoo.de>
2015-04-22 18:15:16 +0200

    Don't implement Collection any longer so it can be serialized easily

--- disunity-core/src/main/java/info/ata4/util/collection/Node.java
+++ disunity-core/src/main/java/info/ata4/util/collection/Node.java
@@ -10,17 +10,15 @@
 package info.ata4.util.collection;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 /**
  *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  * @param <T>
  */
-public abstract class Node<T extends Node> implements Collection<T> {
+public class Node<T extends Node<T>> implements Iterable<T> {
     
     private T parent;
     private final List<T> children = new ArrayList<>();
@@ -33,117 +31,30 @@ public abstract class Node<T extends Node> implements Collection<T> {
         this.parent = parent;
     }
     
-    private void childrenParent(Collection<T> col, Node parent) {
-        for (T child : col) {
-            child.parent(parent);
-        }
-    }
-    
-    @Override
     public int size() {
         return children.size();
     }
-
-    @Override
+    
     public boolean isEmpty() {
         return children.isEmpty();
     }
-
-    @Override
-    public boolean contains(Object o) {
-        return children.contains(o);
+    
+    public void add(T node) {
+        node.parent((T) this);
+        children.add(node);
+    }
+    
+    public boolean remove(T node) {
+        boolean removed = children.remove(node);
+        if (removed) {
+            node.parent(null);
+        }
+        return removed;
     }
 
     @Override
     public Iterator<T> iterator() {
         return new IteratorImpl(children.iterator());
-    }
-
-    @Override
-    public Object[] toArray() {
-        return children.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return children.toArray(a);
-    }
-
-    @Override
-    public boolean add(T e) {
-        e.parent(this);
-        return children.add(e);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        boolean r = children.remove(o);
-        if (r && o instanceof Node) {
-            ((Node) o).parent(null);
-        }
-        return r;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return children.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        boolean b = children.addAll(c);
-        childrenParent(children, this);
-        return b;
-    }
-
-    public boolean addAll(int index, Collection<? extends T> c) {
-        boolean b = children.addAll(index, c);
-        childrenParent(children, this);
-        return b;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        List<T> childrenRemoved = new ArrayList<>(children);
-        boolean b = children.removeAll(c);
-        childrenRemoved.removeAll(children);
-        childrenParent(childrenRemoved, null);
-        return b;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        List<T> childrenRemoved = new ArrayList<>(children);
-        boolean b = children.retainAll(c);
-        childrenRemoved.removeAll(children);
-        childrenParent(childrenRemoved, null);
-        return b;
-    }
-
-    @Override
-    public void clear() {
-        childrenParent(children, null);
-        children.clear();
-    }
-
-    @Override
-    public int hashCode() {
-        return children.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Node<?> other = (Node<?>) obj;
-        if (!Objects.equals(this.children, other.children)) {
-            return false;
-        }
-        return true;
     }
     
     private class IteratorImpl implements Iterator<T> {
