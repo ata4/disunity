2ef199be2f0ca170cf80ab71be0ba44789009790
ata4 <barracuda415@yahoo.de>
2016-08-27 17:42:42 +0200

    Added support for split files

--- Disunity.py
+++ Disunity.py
@@ -2,9 +2,105 @@ import sys
 import os
 import struct
 import glob
+import io
 from uuid import UUID
 from pprint import pprint
 from munch import Munch
+
+class ChunkedFileIO(io.BufferedIOBase):
+
+    class Chunk():
+
+        def __init__(self, path, pos):
+            self.handle = open(path, "rb")
+            self.handle.seek(0, io.SEEK_END)
+            self.size = self.handle.tell()
+            self.handle.seek(0, io.SEEK_SET)
+            self.start = pos
+            self.end = pos + self.size
+
+        def tell(self):
+            return self.start + self.handle.tell()
+
+        def read(self, size=-1):
+            return self.handle.read(size)
+
+        def seek(self, offset):
+            offset -= self.start
+            if offset < 0 or offset > self.size:
+                raise ValueError("Offset out of range:", offset)
+            self.handle.seek(offset, io.SEEK_SET)
+
+        def close(self):
+            self.handle.close()
+
+    chunks = []
+    index = 0
+
+    def __init__(self, paths):
+        pos = 0
+        for path in paths:
+            chunk = self.Chunk(path, pos)
+            # filter out empty chunks
+            if chunk.size == 0:
+                chunk.close()
+                continue
+            pos += chunk.size
+            self.chunks.append(chunk)
+
+    def chunk(self):
+        return self.chunks[self.index]
+
+    def chunk_next(self):
+        if self.index + 1 >= len(self.chunks):
+            return None
+
+        self.index += 1
+        return self.chunk()
+
+    def chunk_find(self, pos):
+        for self.index, chunk in enumerate(self.chunks):
+            if chunk.end >= pos:
+                break
+        return chunk
+
+    def read(self, size=-1):
+        chunk = self.chunk()
+        data = chunk.read(size)
+
+        # get next chunk if required
+        if not data:
+            chunk = self.chunk_next()
+            if chunk:
+                data = chunk.read(size)
+
+        return data
+
+    def seek(self, offset, whence=io.SEEK_SET):
+        chunk = self.chunk()
+
+        # convert relative offset to absolute position
+        pos = offset
+        if whence == io.SEEK_CUR:
+            pos += chunk.tell()
+        elif whence == io.SEEK_END:
+            pos += self.chunks[-1].end
+        elif whence != io.SEEK_SET:
+            raise NotImplementedError()
+
+        # find new chunk if absolute position is outside current chunk
+        if pos < chunk.start or pos > chunk.end:
+            chunk = self.chunk_find(pos)
+
+        chunk.seek(pos)
+
+    def tell(self):
+        return self.chunk().tell()
+
+    def close(self):
+        for chunk in self.chunks:
+            chunk.close()
+        super(ChunkedFileIO, self).close()
 
 class BinaryReader:
 
@@ -218,17 +314,34 @@ def main(argv):
     reader = SerializedFileReader()
 
     for globpath in glob.iglob(path):
-        _, fext = os.path.splitext(globpath)
-        if fext == ".resource" or fext[0:6] == ".split":
-            continue
-
         if os.path.isdir(globpath):
             continue
 
-        print(globpath)
-        with open(globpath, "rb") as file:
-            sf = reader.read(file)
-            pprint(sf)
+        fname, fext = os.path.splitext(globpath)
+        if fext == ".resource":
+            continue
+
+        if fext == ".split0":
+            index = 0
+            splitpath = fname + fext
+            splitpaths = []
+
+            while os.path.exists(splitpath):
+                splitpaths.append(splitpath)
+                index += 1
+                splitpath = fname + ".split%d" % index
+
+            print(splitpaths[0])
+            with ChunkedFileIO(splitpaths) as file:
+                sf = reader.read(file)
+                pprint(sf)
+        elif fext[0:6] == ".split":
+            continue
+        else:
+            print(globpath)
+            with open(globpath, "rb") as file:
+                sf = reader.read(file)
+                pprint(sf)
 
     return 0
 
