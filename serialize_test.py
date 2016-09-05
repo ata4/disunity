ffc417f3a2c1a687244d7af9ad69be1e8214f6fd
ata4 <barracuda415@yahoo.de>
2016-09-05 03:25:01 +0200

    Added some nice stats

--- serialize_test.py
+++ serialize_test.py
@@ -10,26 +10,57 @@ def main(argv):
     app = argv.pop(0)
     path = argv.pop(0)
 
+    num_files_passed = 0
+    num_files_failed = 0
+    num_files_skipped = 0
+    num_objects_passed = 0
+    num_objects_failed = 0
+    num_objects_typeless = 0
+
     for globpath in glob.iglob(path, recursive=True):
         if os.path.isdir(globpath):
             continue
 
-        with pynity.SerializedFile(globpath) as sf:
-            if not sf.valid:
-                continue
-
-            print(globpath)
-            sf.scan_types()
-
-            for path_id in sf.objects:
-                object = sf.read_object(path_id)
-                if not object:
+        try:
+            with pynity.SerializedFile(globpath) as sf:
+                if not sf.valid:
+                    num_files_skipped += 1
                     continue
-                object_name = object.m_Name if "m_Name" in object else ""
-                class_name = object.__class__.__name__
-                print(path_id, class_name, object_name)
-                #object_json = json.dumps(object, indent=2, separators=(',', ': '))
-                #print(object_json)
+
+                print(globpath)
+                sf.scan_types()
+
+                for path_id in sf.objects:
+                    try:
+                        object = sf.read_object(path_id)
+                        if not object:
+                            num_objects_typeless += 1
+                            continue
+
+                        object_name = object.m_Name if "m_Name" in object else ""
+                        class_name = object.__class__.__name__
+                        print(path_id, class_name, object_name)
+
+                        #object_json = json.dumps(object, indent=2, separators=(',', ': '))
+                        #print(object_json)
+
+                        num_objects_passed += 1
+                    except Exception as e:
+                        print(e)
+                        num_objects_failed += 1
+
+                num_files_passed += 1
+        except Exception as e:
+            print(e)
+            num_files_failed += 1
+
+    print()
+    print("Files passed:     %d" % num_files_passed)
+    print("Files failed:     %d" % num_files_failed)
+    print("Files skipped:    %d" % num_files_skipped)
+    print("Objects passed:   %d" % num_objects_passed)
+    print("Objects failed:   %d" % num_objects_failed)
+    print("Objects typeless: %d" % num_objects_typeless)
 
     return 0
 
